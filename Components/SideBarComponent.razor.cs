ce7a7aad31547a6dd8d083be6b17099a4b326208
Guilherme Bley <guilhermecbley@gmail.com>
2025-03-20 21:03:37 -0300

    Add project files.

---
+++ Components/SideBarComponent.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace SmSideBarExample.Components;
+
+public record SideBarItem(
+    string ViewName,
+    string Icon);
+
+public partial class SideBarComponent : ComponentBase
+{
+    private int? _currentActiveIndex;
+    [Parameter]
+    public IReadOnlyList<SideBarItem> Items { get; set; } = [];
+
+    private Task ActiveItemAsync(SideBarItem item, int index)
+    {
+        _currentActiveIndex = index;
+        return Task.CompletedTask;
+    }
+}
